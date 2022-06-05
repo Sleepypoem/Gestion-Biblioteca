b86bb67715fb93befd1e6da77d2bc543ab19885d
Sleepypoem <gallade2014@gmail.com>
2022-06-05 16:09:10 -0600

    Arreglada la navbar que no funcionaba en la vista de moviles.

--- Plantillas/Cabecera.php
+++ Plantillas/Cabecera.php
@@ -71,16 +71,16 @@ $url = "http://" . $_SERVER["HTTP_HOST"] . "/public/Gestion Biblioteca/";
     </style>
 
     <!-- Aqui empieza la NavBar -->
-    <nav class="navbar navbar-expand-xl justify-content-center navbar-dark bg-dark"
+
+    <nav class="navbar navbar-expand-xl justify-content-center navbar-light bg-light navbar-dark bg-dark"
         style="box-shadow: 1px 2px 10px 5px #000000;">
         <div class="container">
             <a class="navbar-brand" href="<?php echo $url . "index.php"; ?>">Biblioteca</a>
-            <button class=" navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-                data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                     <li class="nav-item ">
                         <a class="nav-link" href="<?php echo $url . "secciones/Prestamos.php" ?>">Prestamos </a>
@@ -94,11 +94,14 @@ $url = "http://" . $_SERVER["HTTP_HOST"] . "/public/Gestion Biblioteca/";
                     </li>
 
                 </ul>
-
             </div>
         </div>
     </nav>
 
+    <!-- Este script sirve para que la navbar cambie de tamaño cuando se ve desde moviles -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
 
 </head>
