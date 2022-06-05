9caecfc77bfcca64aefd374f8800374bdbf414d4
Sleepypoem <gallade2014@gmail.com>
2022-06-04 23:33:25 -0600

    Añadidos iconos en el indice

--- index.php
+++ index.php
@@ -1,77 +1,89 @@
 <?php
 include("Plantillas/Cabecera.php");
+
+function obtenerNombrePagina()
+{
+    return "Inicio";
+}
 ?>
 <br>
 
 <h1 class="display-4 text-center">Bienvenido al sistema administrador.</h1>
+<br>
 
 <body>
     <div class="container d-flex justify-content-center index-flex-parent">
         <!-- la primera fila -->
         <div class="card index-element index-flex-child">
-            <div class="card-header text-white text-center bg-dark">
+            <div class="card-header text-center bg-light">
+                <img class="card-image" src="img/users.png" alt="users">
+
+            </div>
+            <div class="card-body card-body-font text-center">
+
+                0
+            </div>
+            <div class="card-footer text-white text-center bg-dark">
                 Usuarios Totales
             </div>
-            <div class="card-body index-font text-center">
-                0
-            </div>
-            <div class="card-footer bg-dark text-muted">
-
-            </div>
         </div>
         <div class="card index-element index-flex-child">
-            <div class="card-header  text-white text-center bg-dark">
+            <div class="card-header text-center bg-light">
+                <img class="card-image" src="img/admins.png" alt="users">
+            </div>
+            <div class="card-body card-body-font text-center">
+                0
+            </div>
+            <div class="card-footer text-white text-center bg-dark ">
                 Administradores
             </div>
-            <div class="card-body index-font text-center">
-                0
-            </div>
-            <div class="card-footer bg-dark text-muted">
-
-            </div>
         </div>
         <div class="card index-element index-flex-child">
-            <div class="card-header text-white text-center bg-dark">
+            <div class="card-header text-center bg-light">
+                <img class="card-image" src="img/books.png" alt="users">
+
+            </div>
+            <div class="card-body card-body-font text-center">
+                0
+            </div>
+            <div class="card-footer text-white text-center bg-dark ">
                 Libros
             </div>
-            <div class="card-body index-font text-center">
-                0
-            </div>
-            <div class="card-footer bg-dark text-muted">
-
-            </div>
         </div>
         <div class="card index-element index-flex-child">
-            <div class="card-header text-white text-center bg-dark">
+            <div class="card-header text-center bg-light">
+                <img class="card-image" src="img/borrowings.png" alt="users">
+
+            </div>
+            <div class="card-body card-body-font text-center">
+                0
+            </div>
+            <div class="card-footer text-white text-center bg-dark ">
                 Prestamos
             </div>
-            <div class="card-body index-font text-center">
-                0
-            </div>
-            <div class="card-footer bg-dark text-muted">
-
-            </div>
         </div>
         <div class="card index-element index-flex-child">
-            <div class="card-header text-white text-center bg-dark">
+            <div class="card-header text-center bg-light">
+                <img class="card-image" src="img/returns.png" alt="users">
+
+            </div>
+            <div class="card-body card-body-font text-center">
+                0
+            </div>
+            <div class="card-footer text-white text-center bg-dark ">
                 Devoluciones pendientes
             </div>
-            <div class="card-body index-font text-center">
-                0
-            </div>
-            <div class="card-footer bg-dark text-muted">
-
-            </div>
         </div>
         <div class="card index-element index-flex-child">
-            <div class="card-header text-white text-center bg-dark">
-                Multas
+            <div class="card-header text-center bg-light">
+                <img class="card-image" src="img/penaltys.png" alt="users">
+
             </div>
-            <div class="card-body index-font text-center">
+            <div class="card-body card-body-font text-center">
                 0
             </div>
-            <div class="card-footer bg-dark text-muted">
-
+            <div class="card-footer text-white text-center bg-dark ">
+                Multas
             </div>
         </div>
     </div>
