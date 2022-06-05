6ef4d53fce267da178a40f1971187960a7cd6179
Sleepypoem <gallade2014@gmail.com>
2022-06-05 17:11:14 -0600

    Removidos los estilos de la cabecera para ponerlos en la hoja de estilos.

--- Plantillas/Cabecera.php
+++ Plantillas/Cabecera.php
@@ -14,61 +14,7 @@ $url = "http://" . $_SERVER["HTTP_HOST"] . "/public/Gestion Biblioteca/";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Gestion de Biblioteca | <?php echo obtenerNombrePagina() ?></title>
-    <style>
-    .index-element {
-        border: 4px solid rgb(204, 68, 0);
-        border-radius: 10px;
-        box-shadow: 0px 8px 20px 3px rgba(112, 112, 112, 0.88);
-    }
-
-    .index-element:hover {
-        transform: scale(1.1);
-    }
-
-    .card-body-font {
-        font-family: "Lucida Sans Unicode", Charcoal, sans-serif;
-        font-size: 115px;
-        letter-spacing: 0.8px;
-        word-spacing: 0.6px;
-        color: #000000;
-        font-weight: 700;
-        text-decoration: none;
-        font-style: normal;
-        font-variant: normal;
-        text-transform: none;
-    }
-
-    .card-image {
-        height: 75px;
-        width: 75px;
-    }
-
-    .card-header-font {
-        font-family: "Courier New", Courier, monospace;
-        font-size: 35px;
-        letter-spacing: -0.8px;
-        word-spacing: 1.2px;
-        color: #000000;
-        font-weight: 700;
-        text-decoration: none solid rgb(68, 68, 68);
-        font-style: normal;
-        font-variant: small-caps;
-        text-transform: none;
-    }
-
-    .index-flex-parent {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(3, 1fr);
-        gap: 40px;
-        flex-wrap: wrap;
-    }
-
-    .index-flex-child {
-        width: 350px;
-        height: 350px;
-    }
-    </style>
+    <link rel="stylesheet" href="<?php echo $url . "css/customStyleSheet.css"; ?>">
 
     <!-- Aqui empieza la NavBar -->
 
