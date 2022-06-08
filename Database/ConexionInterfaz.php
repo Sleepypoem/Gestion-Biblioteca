fe13c60aaadbb9ef00df52b4e322be9a57675e46
Sleepypoem <gallade2014@gmail.com>
2022-06-07 19:45:54 -0600

    Primera commit refactorizando la clase conexion

--- Database/ConexionInterfaz.php
+++ Database/ConexionInterfaz.php
@@ -3,7 +3,7 @@
 interface ConexionInterfaz
 {
     public function conectar();
-    public function insertar();
+    public function insertar(array $data);
     public function actualizar($id, array $data);
     public function borrar($id);
     public function buscar($id);
