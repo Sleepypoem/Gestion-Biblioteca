84a1e3605defd972c38f9681db225356a06f7949
Sleepypoem <gallade2014@gmail.com>
2022-06-11 00:08:14 -0600

    Agregada funcionalidad a la conexion de los autores a la BD

--- classes/Conexiones/AutoresBD.php
+++ classes/Conexiones/AutoresBD.php
@@ -3,4 +3,30 @@
 require_once "ConexionBD.php";
 class AutoresBD extends ConexionBD
 {
+    private $autor;
+
+    public function prepararDatos($autor)
+    {
+        $this->autor = $autor;
+    }
+
+    public function enviaraBD()
+    {
+        $sql = "INSERT INTO autor (nombre, fechaNacimiento, image) VALUES (" . $this->autor->getNombre() . ", " .
+            $this->autor->getFechaDeNacimiento() . ", " . $this->autor->getImagen() . ")";
+
+        $this->enviarDatos($sql);
+    }
+
+    public function editarEnBD($id, $datos)
+    {
+    }
+
+    public function obtenerDeBD()
+    {
+        $sql = "SELECT * FROM autor";
+
+        $listaAutores = $this->consultarDatos($sql);
+        return $listaAutores;
+    }
 }
