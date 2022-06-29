893ed189f75d3a24066552ccdd525d7ac4e53f86
Sleepypoem <gallade2014@gmail.com>
2022-06-29 14:48:21 -0600

    Modificado el index para que redirija a la pagina de login si se ingresa por primera vez, añadida pagina de login

--- index.php
+++ index.php
@@ -1,137 +1,111 @@
 <?php
 /* ***************************************************************** Dependencias ***************************************************************** */
 include_once $_SERVER['DOCUMENT_ROOT'] . "/Gestion Biblioteca/config.php";
-include_once TEMPLATES . "/Cabecera.php";
+include_once TEMPLATES . "/CabeceraLogin.php";
 require_once CONTROLLERS . "/Intermediario.php";
+require_once VIEWS . "/CrearAlertas.php";
 /* ************************************************************************************************************************************************ */
 
-function obtenerNombrePagina()
-{
-    return "Inicio";
-}
-
+/* ******************************************************************* Variables ****************************************************************** */
 $intermediario = new Intermediario();
+$alertas = new CrearAlertas();
+/* ************************************************************************************************************************************************ */
+if ($_POST) {
+    $usuario = $_POST["usuario"];
+    $contrasenia = $_POST["contrasenia"];
 
-$usuarios = count($intermediario->obtenerDeBD("usuario"));
-$libros = count($intermediario->obtenerDeBD("libro"));
-$autores = count($intermediario->obtenerDeBD("autor"));
-$prestamos = count($intermediario->obtenerDeBD("prestamo"));
-$devoluciones = count($intermediario->obtenerDeBD("devolucion"));
+    $sql = "SELECT * FROM usuario WHERE usuario = '$usuario' ";
+    $datosDeBD = $intermediario->consultarConBD($sql);
+    $contraseniaBD = ($datosDeBD !== []) ? $intermediario->consultarConBD($sql)[0]["password"] : 0;
 
+    if (password_verify($contrasenia, $contraseniaBD)) {
+        session_start();
+        $_SESSION["codigo"] = $datosDeBD[0]["codigo"];
+        $_SESSION["nombre"] = $datosDeBD[0]["nombre"];
+        $_SESSION["direccion"] = $datosDeBD[0]["direccion"];
+        $_SESSION["telefono"] = $datosDeBD[0]["telefono"];
+        $_SESSION["imagen"] = $datosDeBD[0]["imagen"];
 
+        header("location:home.php");
+    } else {
+        echo "<br>";
+        echo $alertas->crearAlertaFallo("Error, usuario o contraseña no valido.");
+        header("refresh:2");
+    }
+}
 ?>
 
-<h1 class="display-4 text-center">Bienvenido al sistema, administrador.</h1>
-<br>
+<h1 class="text-dark text-center mt-5">Ingresar al sistema</h1>
 
-<body>
-    <div class="container">
-        <div class="row">
-
-            <div class="col-sm-4">
-
-                <div class="card">
-                    <div class="card-header text-center bg-primary">
-                        <i class="bi bi-person-circle" style="font-size: 3rem; color: white;"></i>
-                    </div>
-                    <div class="card-body card-body-font text-white text-center bg-success">
-                        <?php echo $usuarios ?>
-                    </div>
-                    <div class="card-footer fs-2 text-white text-center bg-primary">
-                        <strong>Usuarios totales</strong>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="col-sm-4">
-
-                <div class="card ">
-                    <div class="card-header text-center bg-primary">
-                        <i class="bi bi-person-video2" style="font-size: 3rem; color: white;"></i>
-                    </div>
-                    <div class="card-body card-body-font text-white text-center bg-success">
-                        1
-                    </div>
-                    <div class="card-footer fs-2 text-white text-center bg-primary">
-                        <strong>Administradores</strong>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="col-sm-4">
-
-                <div class="card ">
-                    <div class="card-header text-center bg-primary">
-                        <i class="bi bi-book" style="font-size: 3rem; color: white;"></i>
-                    </div>
-                    <div class="card-body card-body-font text-white text-center bg-success">
-                        <?php echo $libros ?>
-                    </div>
-                    <div class="card-footer fs-2 text-white text-center bg-primary">
-                        <strong>Libros</strong>
-                    </div>
-                </div>
-
-            </div>
+<div class="container pr-3 pl-3 mt-5">
+    <div class="card">
+        <div class="card-header text-white text-center bg-dark">
+            <img src="<?php echo $config["paths"]["images"]["content"] . "/portada.png" ?>"
+                class="card-img" alt="Imagen de portada" height="400">
 
         </div>
-        <br>
-        <div class="row">
-            <div class="col-sm-4">
-
-                <div class="card ">
-                    <div class="card-header text-center bg-primary">
-                        <i class="bi bi-arrow-up-circle-fill"
-                            style="font-size: 3rem; color: white;"></i>
+        <div class="card-body">
+            <form action="" method="POST">
+                <div class="mb-3">
+                    <label class="form-label">
+                        Ingresa tu usuario
+                    </label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="bi bi-person-circle"></i>
+                            </span>
+                        </div>
+                        <input type="text" name="usuario" class="form-control">
                     </div>
-                    <div class="card-body card-body-font text-white text-center bg-success">
-                        <?php echo $prestamos ?>
-                    </div>
-                    <div class="card-footer fs-2 text-white text-center bg-primary">
-                        <strong>Préstamos</strong>
-                    </div>
+                    <small id="helpId" class="form-text text-muted">
+                        Si no cuentas con un usuario comunicate con el administrador.
+                    </small>
                 </div>
 
-            </div>
-
-            <div class="col-sm-4">
-
-                <div class="card ">
-                    <div class="card-header text-center bg-primary">
-                        <i class="bi bi-arrow-down-circle-fill"
-                            style="font-size: 3rem; color: white;"></i>
-                    </div>
-                    <div class="card-body card-body-font text-white text-center bg-success">
-                        <?php echo $devoluciones ?>
-                    </div>
-                    <div class="card-footer fs-2 text-white text-center bg-primary">
-                        <strong>Devoluciones</strong>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="col-sm-4">
-
-                <div class="card ">
-                    <div class="card-header text-center bg-primary">
-                        <i class="bi bi-cash-coin" style="font-size: 3rem; color: white;"></i>
-                    </div>
-                    <div class="card-body card-body-font text-white text-center bg-success">
-                        0
-                    </div>
-                    <div class="card-footer fs-2 text-white text-center bg-primary">
-                        <strong>Multas</strong>
+                <div class="mb-3">
+                    <label class="form-label">
+                        Ingresa tu contraseña
+                    </label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="bi bi-key"></i>
+                            </span>
+                        </div>
+                        <input type="password" class="form-control" name="contrasenia"
+                            id="contrasenia" />
+                        <span class="input-group-text">
+                            <i class="bi bi-eye-slash" id="visibilidadContrasenia"></i>
+                        </span>
                     </div>
                 </div>
-
-            </div>
         </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-success">Ingresar</button>
+        </div>
+        </form>
     </div>
-</body>
+    <div class="card-footer text-muted bg-dark">
+    </div>
+</div>
+</div>
+
+<script>
+//tomamos el icono y el campo de la contraseñapor su id
+const visibilidadContrasenia = document.querySelector("#visibilidadContrasenia");
+const contrasenia = document.querySelector("#contrasenia");
+
+//añadimos un escuchador para saber cuando se hace click en el icono
+visibilidadContrasenia.addEventListener("click", function() {
+    // cambiamos el atributo del campo de contraseña de "password" a "texto" para hacerla visible
+    const type = contrasenia.getAttribute("type") === "password" ? "text" : "password";
+    contrasenia.setAttribute("type", type);
+
+    // cambiamos tambien el icono
+    this.classList.toggle("bi-eye");
+});
+</script>
 
 <?php
 /* ***************************************************************** Dependencias ***************************************************************** */
